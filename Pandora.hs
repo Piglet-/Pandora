1da49b38e2ecd3dceb0e870bc0a91d58c059a74d
Piglet- <cferreira6594@gmail.com>
2016-05-03 21:31:12 -0430

    Agregado uso de flags (solo debug esta muy primitivo)

--- Pandora.hs
+++ Pandora.hs
@@ -8,6 +8,19 @@ main = do
         then getContents
         else readFile (head args)
     case scanner str of
-        --Right lexs -> mapM_ fPrint lexs
-        Right lexs -> print (parse lexs)   
+        Right lexs ->
+            if null (tail args)
+                then print (parse lexs) 
+                else case head (tail args) of
+                    "-l" -> mapM_ fPrint lexs
+                    "-p" -> print (parse lexs) 
+                    _    -> print help
         Left error -> print error
+
+help :: String
+help = "Los flags permitidos por ahora son -l (lexer) y -p (parser)"
+
+--    case scanner str of
+        --Right lexs -> mapM_ fPrint lexs
+--        Right lexs -> print (parse lexs)   
+        --Left error -> print error
