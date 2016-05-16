386ebdbbd7a1eb3d18893f7d4787a9a7685fbc9f
Piglet- <cferreira6594@gmail.com>
2016-05-15 23:00:57 -0430

    Se agregaron los tipos a la ST

--- SymbolTable.hs
+++ SymbolTable.hs
@@ -20,14 +20,14 @@ import Data.Maybe (fromJust)
 
 -- una entrada (o valor en un hash) es el tipo (en string)
 -- y la posición de una variable dentro de la tabla de símbolos
-data Entry = Entry String Position 
+data Entry = Entry String Position deriving (Show)
 
 -- un scope es un entero que representa el nivel de anidamiento,
 -- y una tupla con la posicion inicial y final del scope
-data Scope = Scope Int (Position, Position)
+data Scope = Scope Int (Position, Position) deriving(Show)
 
 -- tipo de datos para la tabla de simbolos
-data SymbolTable = SymbolTable Int (DMap.Map String String) (DS.Seq (SymbolTable)) 
+data SymbolTable = SymbolTable Scope (DMap.Map String Entry) (DS.Seq (SymbolTable)) 
 
 -- instancia show de la tabla de simbolos
 instance Show SymbolTable where
@@ -35,14 +35,14 @@ instance Show SymbolTable where
 
 -- mestra las tablas con identacion
 showTable :: SymbolTable -> String
-showTable (SymbolTable t maps childrens) = 
+showTable (SymbolTable (Scope t s) maps childrens) = 
     (tabs t) ++ "Level: " ++ show t ++ "\n" ++
     (tabs t) ++ "Scope:\n" ++
     (showHash maps t) ++ concat (toList (fmap showTable (DS.reverse childrens))) 
         where tabs t = concat $ replicate t "\t"
 
 -- muestra los elementos internos de la tabla
-showHash :: (DMap.Map String String) -> Int -> String
+showHash :: (DMap.Map String Entry) -> Int -> String
 showHash m t = showListC (DMap.keys m) t
 
 showListC :: [String] -> Int -> String
@@ -52,7 +52,7 @@ showListC (x:xs) t = (concat $ replicate t "\t") ++
 
 -- tipo de datos del camino
 data Breadcrumbs = 
-    Breadcrumbs Int (DMap.Map String String) (DS.Seq (SymbolTable)) (DS.Seq (SymbolTable)) 
+    Breadcrumbs Scope (DMap.Map String Entry) (DS.Seq (SymbolTable)) (DS.Seq (SymbolTable)) 
         deriving (Show) 
 
 -- zipper que modela una tabla de simbolos y el camino
@@ -77,8 +77,8 @@ tothetop (fcs, []) = (fcs, [])
 tothetop bc = tothetop $ fromJust (goBack bc)
 
 -- busca el simbolo en toda la tabla
-lookupS :: String -> Zipper -> Maybe (String, Int) 
-lookupS k (SymbolTable i m sts, bs) = 
+lookupS :: String -> Zipper -> Maybe (Entry, Scope) -- No se si es bueno devolver el  
+lookupS k (SymbolTable i m sts, bs) =                -- scope o el entero
                 case DMap.lookup k m of
                         Nothing -> if null bs 
                                     then Nothing
@@ -86,17 +86,17 @@ lookupS k (SymbolTable i m sts, bs) =
                         Just v  -> Just (v, i)
 
 -- busca el simbolo solo en el alcace actual
-lookupS' :: String -> Zipper -> Maybe String
+lookupS' :: String -> Zipper -> Maybe Entry
 lookupS' k (SymbolTable i m sts, bs) = DMap.lookup k m 
 
 -- inserta un nuevo simbolo
-insertS :: String -> String -> Zipper -> Zipper 
+insertS :: String -> Entry -> Zipper -> Zipper 
 insertS k v (SymbolTable i m sts, bs) = (SymbolTable i (DMap.insert k v m) sts, bs) 
 
 -- inserta una nueva tabla
 insertT :: Zipper -> Zipper
-insertT (SymbolTable i m sts, bs) = (SymbolTable i m (st DS.<| sts), bs)
-	where st = (SymbolTable (succ i) DMap.empty (DS.empty))
+insertT (SymbolTable (Scope i p) m sts, bs) = (SymbolTable (Scope i p) m (st DS.<| sts), bs)
+	where st = (SymbolTable (Scope (succ i) p) DMap.empty (DS.empty))
 
 -- coloca una tabla en el zipper
 focus :: SymbolTable -> Zipper
@@ -107,7 +107,7 @@ defocus :: Zipper -> SymbolTable
 defocus (st, bs) = st  
 
 -- inicia una tabla como vacia
-emptyST :: Int -> SymbolTable 
+emptyST :: Scope -> SymbolTable 
 emptyST i = SymbolTable i (DMap.empty) (DS.empty)
 
 -- habrir un nuevo alcance
