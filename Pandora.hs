8a2e4658338520f8ae45e395aa6d09ee63487165
dayanar23 <dayana2301@gmail.com>
2016-05-18 08:10:16 -0400

    impresion de bitacora con errores

--- Pandora.hs
+++ Pandora.hs
@@ -2,7 +2,7 @@ import Lexer
 import Parser
 import System.Environment (getArgs)
 import SymbolTable
-import Tokens
+import Type
 import Control.Monad.RWS
 
 main = do
@@ -17,6 +17,8 @@ main = do
                         then mapM_ fPrint (filter isTokenError lexs)
                         else do let (state, bita) = execRWS (parse lexs) "" (emptyZipper, emptyZipper)
                                 print $ defocus $ fst state
+                                print $ defocus $ snd state
+                                print $ filterBit bita
                             --print $ drop 2 (show (parse lexs) ++ "Accepted") 
                 else case head (tail args) of
                     "-l" -> if any isTokenError lexs 
@@ -27,6 +29,7 @@ main = do
                                 else do let (state, bita) = execRWS (parse lexs) "" (emptyZipper, emptyZipper)
                                         print $ defocus $ fst state
                                         print $ defocus $ snd state
+                                        print bita
                                     --print $ drop 2 (show (parse lexs) ++ "Accepted") 
                     _    -> print help
         Left error -> print error
@@ -37,6 +40,11 @@ help = "Los flags permitidos por ahora son -l (lexer) y -p (parser)"
 emptyZipper :: Zipper
 emptyZipper = focus $ emptyST emptyScope
 
+filterBit :: [Binnacle] -> [Binnacle]
+filterBit []        = []
+filterBit (b:bs)    =  if b == (Right "") 
+                            then filterBit bs
+                            else b:(filterBit bs)
 --    case scanner str of
         --Right lexs -> mapM_ fPrint lexs
 --        Right lexs -> print (parse lexs)   
